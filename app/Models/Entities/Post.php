633f9cf18c4dcf717b1ae5ee3711017c31797c9b
et397 <b10812031@crepowermay.com>
2024-05-07 09:28:19 +0800

    變更.env建立mysql連線，增加blog資料庫，新增Post migration，建立Post資料表與實體

---
+++ app/Models/Entities/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Entities;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'user_id' ,'title', 'content'
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo('app\Models\Entities\User');
+    }
+}
