fe71eef7fd57d13981ac852a77a9e7e5b33eedba
et397 <b10812031@crepowermay.com>
2024-05-07 10:11:35 +0800

    新增post controller，新增post route，新增repository資料夾

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,3 +6,12 @@ abstract class Controller
 {
     //
 }
+
+// 進入test function後，回傳「resources\views\test.blade.php」的頁面
+class TestController extends Controller
+{
+    public function test()
+    {
+        return view('test');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+#用get方法，導向「/test」的網址，並執行TestController的test方法，回傳業ai驗
+Route::get('test', 'TestController@test');
